823c9db17be5198056fb48f915dbdf2a23924cdc
Carlos P <carlitospp2510@hotmail.com>
2024-05-16 05:39:33 -0500

    KashVue v1.1 - login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:StartComponent},
+    {path:'login-register',component:StartComponent}
+];
